c3ac56b924c1032a60ebd4cf7f9768a2d865ad2e
PRABHUGOUDA <121365975+prabhugouda48@users.noreply.github.com>
2024-12-07 10:22:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Frontend Developer", "Python Developer", "Django Frameworke","Web Developer"],
+    strings:["Frontend Developer", "Python Developer","Web Developer"],
     typeSpeed : 10,
     backSpeed : 100,
     backDelay : 1000,
