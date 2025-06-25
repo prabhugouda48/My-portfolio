ac164c95a2e22682d690998d5da9fa7e64691b2a
PRABHUGOUDA <121365975+prabhugouda48@users.noreply.github.com>
2025-06-25 10:23:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ var typed = new Typed(".text",{
     backDelay : 1000,
     loop : true
 });
+    function submitForm() {
+        const form = document.getElementById('myForm');
+        let body = 'Name: ' + form.name.value + '\n';
+        body += 'Message: ' + form.message.value;
+        body += 'subject: ' + form.Subject.value;
+        body += 'email: ' + form.Email.value;
+        body += 'number: ' + form.MobileNumber.value;
+
+        const mailtoLink = `mailto:prabhugouda48@gmail.com?subject=Form Submission&body=${encodeURIComponent(body)}`;
+        window.location.href = mailtoLink;
+    }
